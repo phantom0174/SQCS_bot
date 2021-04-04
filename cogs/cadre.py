73d2fb038f5825c3f15a6056c4af2ff602e408af
phantom0174 <darrenhsiou@gmail.com>
2021-04-04 22:16:06 +0800

    cadre update

--- cogs/cadre.py
+++ cogs/cadre.py
@@ -2,6 +2,7 @@ from core.classes import Cog_Extension
 from core.setup import jdata, client
 from discord.ext import commands
 import core.functions as func
+import os
 
 
 class Cadre(Cog_Extension):
@@ -12,82 +13,88 @@ class Cadre(Cog_Extension):
 
     @ca.command()
     async def apply(self, ctx, cadre):
-        if ctx.channel.id != 774794670034124850:
+        appl = ctx.author  # applicant
+
+        await func.report(self.bot, f'[CMD EXECUTED][ca][apply][{appl.name}][{appl.id}]\n'
+                                    f'[cadre: {cadre}]')
+
+        if ctx.channel.name != '📝幹部申請區':
             return
 
-        applicant = ctx.author.id
-        if func.cadre_trans(cadre) == -1:
-            await ctx.send(f':exclamation: There are no cadre called {cadre}!')
+        if cadre not in ['副召', '網管', '議程', '公關', '美宣']:
+            await ctx.send(content=f':exclamation: {appl.mention}, 沒有名為 `{cadre}` 的職位！', delete_after=5.0)
             return
 
         cadre_cursor = client["cadre"]
-        data = cadre_cursor.find_one({"_id": applicant})
+        data = cadre_cursor.find_one({"_id": appl.id})
 
-        if data is not None:
-            await ctx.author.send(f':exclamation: You\'ve already made a application!\n'
-                                  f'Id: {data["_id"]}, Apply Cadre: {data["apply_cadre"]}, Apply Time: {data["apply_time"]}')
+        if data.count() != 0:
+            await appl.send(f':exclamation: {appl.mention} (id: {data["_id"]}),\n'
+                            f'您已經於 {data["apply_time"]} 申請 `{data["apply_cadre"]}` 職位！\n'
+                            f'請確認是否發生以下狀況 `重複申請；同時申請兩職位；申請錯誤`\n'
+                            f'如有疑問請洽 @總召')
             return
 
         apply_time = func.now_time_info('whole')
-        apply_info = {"_id": applicant, "apply_cadre": cadre, "apply_time": apply_time}
+        apply_info = {"_id": appl, "apply_cadre": cadre, "apply_time": apply_time}
         cadre_cursor.insert_one(apply_info)
 
-        await ctx.send(f':white_check_mark: Application committed!\n'
-                       f'Id: {applicant}, Apply Cadre: {cadre}, Apply Time: {apply_time}')
-
-        await func.getChannel(self.bot, '_Report').send(
-            f'[Command]Group ca - apply used by member {applicant}. {func.now_time_info("whole")}')
+        await appl.send(f':white_check_mark: 我收到你的申請了！請耐心等待\n'
+                        f'申請人id: {appl}, 申請職位: {cadre}, 申請時間: {apply_time}')
 
     @ca.command()
     @commands.has_any_role('總召', 'Administrator')
     async def list(self, ctx):
+        await func.report(self.bot, f'[CMD EXECUTED][ca][list][{ctx.author.name}][{ctx.author.id}]')
 
         cadre_cursor = client["cadre"]
         data = cadre_cursor.find({})
 
+        if data.count() == 0:
+            await ctx.send(':exclamation: There is no data in the list!')
+            return
+
         apply_info = str()
         for item in data:
-            member_name = await self.bot.guilds[0].fetch_member(item["_id"])
-            if member_name is None:
-                member_name = await self.bot.fetch_user(item["_id"])
-
+            member_name = await ctx.guild.fetch_member(item["_id"])
             apply_info += f'{member_name}({item["_id"]}): {item["apply_cadre"]}, {item["apply_time"]}\n'
 
-        if len(apply_info) == 0:
-            apply_info = ':exclamation: There are no application!'
+            if len(apply_info) > 1600:
+                await ctx.send(apply_info)
+                apply_info = ''
 
-        await ctx.author.send(apply_info)
-
-        await func.getChannel(self.bot, '_Report').send(
-            f'[Command]Group ca - list used by member {ctx.author.id}. {func.now_time_info("whole")}')
+        if len(apply_info) > 0:
+            await ctx.author.send(apply_info)
 
     @ca.command()
     @commands.has_any_role('總召', 'Administrator')
     async def permit(self, ctx, permit_id: int):
+        await func.report(self.bot, f'[CMD EXECUTED][ca][permit][{ctx.author.name}][{ctx.author.id}]\n'
+                                    f'[permit_id: {permit_id}]')
 
         cadre_cursor = client["cadre"]
         data = cadre_cursor.find_one({"_id": permit_id})
 
         if data.count() == 0:
-            await ctx.send(f':exclamation: There are no applicant whose id is {permit_id}!')
+            await ctx.send(f':exclamation: There exists no applicant whose id is {permit_id}!')
             return
 
         member = await ctx.guild.fetch_member(data["_id"])
 
         await ctx.author.send(
-            f':white_check_mark: You\'ve permitted user {member.name} to join cadre {data["apply_cadre"]}!\n'
-            f'https://discord.gg/QTS66u8QhG\n'
-            f'此為幹部群的連結，請在加入之後使用指令領取屬於你的身分組')
-        await member.send(f':white_check_mark: You\'ve been permitted to join cadre {data["apply_cadre"]}!')
+            f':white_check_mark: You\'ve permitted user {member.name} to join cadre {data["apply_cadre"]}!')
+
+        await member.send(f':white_check_mark: 您於 {data["apply_time"]} 申請 {data["apply_cadre"]} 的程序已通過！\n'
+                          f'此為幹部群的連結，請在加入之後使用指令領取屬於你的身分組\n'
+                          f'{os.environ.get("Working_link")}')
 
         cadre_cursor.delete_one({"_id": data["_id"]})
-
-        await func.getChannel(self.bot, '_Report').send(
-            f'[Command]Group ca - permit used by member {ctx.author.id}. {func.now_time_info("whole")}')
 
     @ca.command()
     @commands.has_any_role('總召', 'Administrator')
     async def search(self, ctx, search_id: int):
+        await func.report(self.bot, f'[CMD EXECUTED][ca][search][{ctx.author.name}][{ctx.author.id}]\n'
+                                    f'[search_id: {search_id}]')
 
         cadre_cursor = client["cadre"]
         data = cadre_cursor.find_one({"_id": search_id})
@@ -99,26 +106,21 @@ class Cadre(Cog_Extension):
         member = await ctx.guild.fetch_member(data["_id"])
         await ctx.send(f'{member.name}: {data["apply_cadre"]}, {data["apply_time"]}')
 
-        await func.getChannel(self.bot, '_Report').send(
-            f'[Command]Group ca - search used by member {ctx.author.id}. {func.now_time_info("whole")}')
-
     @ca.command()
     async def remove(self, ctx, delete_id: int):
+        await func.report(self.bot, f'[CMD EXECUTED][ca][remove][{ctx.author.name}][{ctx.author.id}]\n'
+                                    f'[delete_id: {delete_id}]')
+
         cadre_cursor = client["cadre"]
         data = cadre_cursor.find_one({"_id": delete_id})
 
         if data.count() == 0:
-            await ctx.send(f':exclamation: There are no applicant whose Id is {delete_id}!')
+            await ctx.send(f':exclamation: There exists no applicant whose id is {delete_id}!')
 
-        member_name = await self.bot.guilds[0].fetch_member(data["_id"])
-        if member_name is None:
-            member_name = await self.bot.fetch_user(data["_id"])
+        member_name = await ctx.guild.fetch_member(data["_id"])
 
         cadre_cursor.delete_one({"_id": delete_id})
         await ctx.send(f'Member {member_name}({delete_id})\'s application has been removed!')
-
-        await func.getChannel(self.bot, '_Report').send(
-            f'[Command]Group ca - remove used by member {ctx.author.id}. {func.now_time_info("whole")}')
 
 
 def setup(bot):
--- core/functions.py
+++ core/functions.py
@@ -53,9 +53,10 @@ def create_embed(Title, thumbnail, Color, FieldsName, Values):
     return embed
 
 
-def getChannel(bot, target):
-    if target == '_Report':
-        return discord.utils.get(bot.guilds[1].text_channels, name='sqcs-report')
+def report(bot, content):
+    report_channel = discord.utils.get(bot.guilds[1].text_channels, name='sqcs-report')
+    msg = content + '\n' + f'[{now_time_info("whole")}]'
+    await report_channel.send(msg)
 
 
 async def get_time_title(hour):
@@ -111,42 +112,3 @@ def score_weight_update(t_score, avr_score, max_score, min_score):
     pt1 = float(1/2)
     pt2 = float(3/(2*(1 + pow(math.e, -5 * alpha + math.log(2)))))
     return pt1 + pt2
-
-
-def cadre_trans(cadre):
-    if cadre == '副召':
-        return 1
-    if cadre == 1:
-        return '副召'
-
-    if cadre == '網管':
-        return 2
-    if cadre == 2:
-        return '網管'
-
-    if cadre == '議程':
-        return 3
-    if cadre == 3:
-        return '議程'
-
-    if cadre == '公關':
-        return 4
-    if cadre == 4:
-        return '公關'
-
-    if cadre == '美宣':
-        return 5
-    if cadre == 5:
-        return '美宣'
-
-    if cadre == '管理':
-        return 6
-    if cadre == 6:
-        return '管理'
-
-    if cadre == '一般':
-        return 7
-    if cadre == 7:
-        return '一般'
-
-    return -1
