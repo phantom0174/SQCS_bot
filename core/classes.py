2db7b18d3ef620a2824e00c8a7d32bb090b9b3ff
phantom0174 <darrenhsiou@gmail.com>
2021-04-12 21:21:02 +0800

    Update classes.py

--- core/classes.py
+++ core/classes.py
@@ -13,21 +13,30 @@ class JsonApi:
     def __init__(self):
         self.link_header = 'https://api.jsonstorage.net/v1/json/'
 
+        self.abbreviation_dict = {
+            "sta": "StaticSettingJson",
+            "dyn": "DynamicSettingJson",
+            "hum": "HumanityExtensionJson",
+            "nt": "NTJson"
+        }
+
         # json link switcher
         self.json_links = str(os.environ.get("JsonApiLinks"))
         self.link_dict = requests.get(self.link_header + self.json_links).json()
 
     def get_json(self, name):
-        if name not in self.link_dict.keys():
+        if name not in self.abbreviation_dict.keys():
             error_json = {"type": "get_json_error"}
             return error_json
 
-        response = requests.get(self.link_header + self.link_dict[name])
+        full_name = self.abbreviation_dict[name]
+        response = requests.get(self.link_header + self.link_dict[full_name])
         return response.json()
 
     def put_json(self, name, alter_json):
-        if name not in self.link_dict.keys():
+        if name not in self.abbreviation_dict.keys():
             error_json = {"type": "put_json_error"}
             return error_json
 
-        update = requests.put(self.link_header + self.link_dict[name], json=alter_json)
+        full_name = self.abbreviation_dict[name]
+        update = requests.put(self.link_header + self.link_dict[full_name], json=alter_json)
