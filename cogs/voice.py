f43ce7e33733d7975011d84a84adece2d9ac5823
phantom0174 <darrenhsiou@gmail.com>
2021-04-27 23:31:08 +0800

    voice channel protection

--- cogs/voice.py
+++ cogs/voice.py
@@ -1,5 +1,5 @@
 from discord.ext import commands
-from core.classes import CogExtension
+from core.classes import CogExtension, JsonApi
 import asyncio
 
 
@@ -26,6 +26,39 @@ class Voice(CogExtension):
         await message.delete()
 
         for member in voice_channel.members:
+            await member.move_to(None)
+
+    @voice.command()
+    async def protect_add(self, ctx, channel_id: int):
+        dyn_json = JsonApi().get_json('DynamicSetting')
+
+        if channel_id in dyn_json["voice_in_protect"]:
+            return await ctx.send(
+                ':exclamation: The channel is already in protection;\n'
+                'or there exists no channel with this id'
+            )
+
+        dyn_json["voice_in_protect"].append(channel_id)
+        JsonApi().put_json('DynamicSetting', dyn_json)
+
+    @voice.command()
+    async def protect_remove(self, ctx, channel_id: int):
+        dyn_json = JsonApi().get_json('DynamicSetting')
+
+        if channel_id not in dyn_json["voice_in_protect"]:
+            return await ctx.send(
+                ':exclamation: The channel is already out of protection;\n'
+                'or there exists no channel with this id'
+            )
+
+        dyn_json["voice_in_protect"].remove(channel_id)
+        JsonApi().put_json('DynamicSetting', dyn_json)
+
+    @commands.Cog.listener()
+    async def on_voice_state_update(self, member, before, after):
+        voice_in_protect = JsonApi().get_json('DynamicSetting')["voice_in_protect"]
+
+        if after.channel is not None and after.channel in voice_in_protect:
             await member.move_to(None)
 
 
