866cf38c2704a744a7079cc77c816e14c72520a7
phantom0174 <darrenhsiou@gmail.com>
2021-05-03 22:40:28 +0800

    Update text.py

--- cogs/text.py
+++ cogs/text.py
@@ -11,7 +11,6 @@ class Text(CogExtension):
 
     # delete message
     @text.command()
-    @commands.has_any_role('總召', 'Administrator')
     async def clear(self, ctx, msg_id: int):
         find = bool(False)
         while not find:
@@ -43,6 +42,40 @@ class Text(CogExtension):
 
             if msg.id == end_id:
                 break
+
+    @text.command(aliases=['add'])
+    async def add_member(self, ctx, member: discord.Member):
+        perms = ctx.channel.overwrites_for(member)
+
+        perms.view_channel = True
+        perms.read_messages = True
+        perms.add_reactions = True
+        perms.send_messages = True
+        perms.read_message_history = True
+        perms.attach_files = True
+        perms.embed_links = True
+
+        await ctx.channel.set_permissions(member, overwrite=perms)
+        await ctx.send(
+            f':white_check_mark: Successfully added {member.display_name} to this channel!'
+        )
+
+    @text.command(aliases=['remove'])
+    async def remove_member(self, ctx, member: discord.Member):
+        perms = ctx.channel.overwrites_for(member)
+
+        perms.view_channel = None
+        perms.read_messages = None
+        perms.add_reactions = None
+        perms.send_messages = None
+        perms.read_message_history = None
+        perms.attach_files = None
+        perms.embed_links = None
+
+        await ctx.channel.set_permissions(member, overwrite=perms)
+        await ctx.send(
+            f':white_check_mark: Successfully removed {member.display_name} from this channel!'
+        )
 
 
 def setup(bot):
