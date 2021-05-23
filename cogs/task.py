1a693c3d9c1ad54acc8da7926fdf02d66803f21c
phantom0174 <darrenhsiou@gmail.com>
2021-05-23 14:29:26 +0800

    Local logs upload

--- cogs/task.py
+++ cogs/task.py
@@ -1,4 +1,4 @@
-from core.db import self_client, fluctlight_client
+from core.db import self_client, fluctlight_client, JsonApi
 from core.utils import Time
 from cogs.sqcs_plugin.quiz import quiz_start, quiz_end
 import discord
@@ -14,7 +14,8 @@ class Task(CogExtension):
         self.quiz_set_cursor = self_client["QuizSetting"]
 
         self.quiz_auto.start()
-        self.role_check.start()
+        self.role_update_check.start()
+        self.local_log_upload.start()
 
     @tasks.loop(minutes=50)
     async def quiz_auto(self):
@@ -39,7 +40,7 @@ class Task(CogExtension):
             await report_channel.send(f'[AUTO QUIZ END][{Time.get_info("whole")}]')
 
     @tasks.loop(hours=2)
-    async def role_check(self):
+    async def role_update_check(self):
         await self.bot.wait_until_ready()
 
         guild = self.bot.get_guild(743507979369709639)
@@ -64,6 +65,27 @@ class Task(CogExtension):
                     await member.remove_roles(neutral_role)
                     await member.add_roles(auto_role)
                     await member.send(':partying_face: 恭喜！你已升級為自由量子！')
+
+    @tasks.loop(minutes=30)
+    async def local_log_upload(self):
+        with open('./bot.log', mode='r', encoding='utf8') as temp_file:
+            local_logs = temp_file.read().split('\n')
+
+        if local_logs is None:
+            return
+
+        while str('') in local_logs:
+            local_logs.remove('')
+
+        # upload logs
+        log_json = JsonApi().get('CmdLogging')
+        for log in local_logs:
+            log_json['logs'].append(log)
+        JsonApi().put('CmdLogging', log_json)
+
+        # flush local logs
+        with open('./bot.log', mode='w', encoding='utf8') as temp_file:
+            temp_file.write('')
 
 
 def setup(bot):
