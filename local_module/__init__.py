2e29ed770f266ca83592bd164c2f42dc2d40481f
phantom0174 <darrenhsiou@gmail.com>
2021-08-11 17:07:01 +0800

    fix import

--- local_module/__init__.py
+++ local_module/__init__.py
@@ -1,3 +1,3 @@
-from .uplink_python.uplink import Uplink
-from .uplink_python.errors import BucketNotEmptyError, BucketNotFoundError
-from .uplink_python.module_classes import ListObjectsOptions
+from .uplink_python.uplink_python.uplink import Uplink
+from .uplink_python.uplink_python.errors import BucketNotEmptyError, BucketNotFoundError
+from .uplink_python.uplink_python.module_classes import ListObjectsOptions
